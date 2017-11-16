1e743e7dc2d104e0ee9dde17350155a75b6a94f2
ryanosaurus-tex <rmw.inbox@gmail.com>
2017-11-16 13:43:23 -0600

    ajax & handlebars added

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,7 +1,27 @@
 // document ready
 $(function(){
-  $("#searchButton").on("click", function(){
+  $("#searchButton").on("click", function(event){
     console.log("on click function works");
+
+    event.preventDefault();
+
+    var newSearch = {
+      name: $("#searchBox").val().trim()
+    };
+
+    $.ajax('/all/products', {
+      type: "get",
+      data: newSearch
+    }).done(function(response){
+      console.log("created a new search");
+
+      var results = response.data;
+
+      console.log(results);
+
+      
+      }
+    );
   });
 
 }); // /document ready
